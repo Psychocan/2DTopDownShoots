69b4e2ebc0349fd7a328479329670b569d78a1b7
AIE\s170999 <sican98@gmail.com>
2018-11-19 13:29:57 +1100

    Updated the player script and attached it to the prefab

--- Assets/OurAssets/Scripts/MeleeWeapon.cs
+++ Assets/OurAssets/Scripts/MeleeWeapon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour {
+public class MeleeWeapon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/OurAssets/Scripts/Player.cs
+++ Assets/OurAssets/Scripts/Player.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : LivingEntity {
 
-	// Use this for initialization
-	void Start () {
-		
+    [Tooltip("The players melee weapon")]
+    [SerializeField] private MeleeWeapon sword;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashCooldown;
+
+    private Rigidbody2D rb2D;
+    private Vector2 moveVelocity;
+    private Vector2 lastVel;
+    private float lastTime;
+    private float lerpTime;
+
+    protected override void Start () {
+        base.Start();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+    protected override void Update () {
+        base.Update();
+        if (!Dead) {
+            Move();
+            Attack();
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void FixedUpdate() {
+        rb2D.MovePosition(rb2D.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void Move() {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (moveInput.sqrMagnitude <= 0) {
+            if (moveVelocity.sqrMagnitude > 0) { moveVelocity = Vector2.Lerp(lastVel, Vector2.zero, lerpTime); lerpTime += Time.deltaTime; }
+        }
+
+        else { moveVelocity = moveInput.normalized * speed; lastVel = moveVelocity; lerpTime = 0; }
+        
+    }
+
+    private void Attack() {
+        if (!Input.GetMouseButton(0))
+            return;
+        //if(sword.canSwing) {
+            //sword.Swing();
+        //}
+    }
+
+
+
 }
